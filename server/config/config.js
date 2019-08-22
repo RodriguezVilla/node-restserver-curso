a70277a360e12db3b1c58ad460e8165641ecc18e
RubenRV <ruben.rodriguez.villa@gmail.com>
2019-08-22 19:53:57 +0200

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,21 @@
 // =================
 //  Puerto
 // =================
-
 process.env.PORT = process.env.PORT || 3000;
+
+// =================
+//  Entorno
+// =================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =================
+//  Base de datos
+// =================
+let urlDB;
+
+if (process.env.NODE_ENV == 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafeton';
+} else {
+    urlDB = 'mongodb+srv://strider:klGwyYsa9mxqT5qk@cluster0-kczwm.mongodb.net/cafeton';
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,44 +1,30 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-})
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    }
 
-    res.json({
-        persona: body
+// mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
+mongoose.connect(process.env.URLDB, { useCreateIndex: true, useNewUrlParser: true },
+    (err, res) => {
+        if (err) throw err;
+
+        console.log('Bases de datos ONLINE');
     });
-})
 
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id: id
-    });
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-})
 
 app.listen(process.env.PORT, () => {
     console.log("Escuchando puerto: ", process.env.PORT);
