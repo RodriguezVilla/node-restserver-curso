1ba05b7980438b4537921dc952ac81c84bdda620
RubenRV <ruben.rodriguez.villa@gmail.com>
2019-08-25 18:26:29 +0200

    Corregida errata en en el post de creación usuario

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -46,7 +46,7 @@ app.get('/usuario', verificaToken, (req, res) => {
 app.post('/usuario', [verificaToken, verificaAdmin_Role], function(req, res) {
     let body = req.body;
 
-    let usuario = new Usuario({
+    let usuarioDB = new Usuario({
         nombre: body.nombre,
         email: body.email,
         // Passwword plano
